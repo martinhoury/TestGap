2dd03f594ce24cb53620afdc9e624ee6cb50a851
martinhoury <martinhoury@gmail.com>
2014-11-16 13:00:20 +0100

    supression du surplus

--- Projet_phonegap/localisationandpluging.js
+++
@@ -1,9 +0,0 @@
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		var infopos= "<h2>Position déterminée<br></h2>";
-		infopos+= "Latitude : "+position.coords.latitude+"<br>";
-		infopos+= "Longitude: "+position.coords.longitude+"<br>";
-		infopos+= "Altitude : "+position.coords.altitude+"<br>";
-		document.getElementById("localisation").innerHTML= infopos;
-	});
-}
